dd68a9dd7a5b49fe850fa0179a4f3181b85cb320
Ali Raza <161970819+ALiRazax1@users.noreply.github.com>
2024-10-31 23:56:09 +0500

    make it interactive

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // JavaScript Assignment 06 (Random Password Generator)
  
     let charters = "asdfghjklqwertyuiopzxcvbnmASDFGHJKLPQWERTYUIOZXCVBNM0123456789!@#$%^&*()_+";
-    let passwordLength = +prompt("Enter Password lenght",8);
-    let password = "";
-    for (let i = 0; i < passwordLength; i++) {
+    let passwordSlider = document.querySelector('#customRange1')
+    let passwordLength  = document.querySelector('#password-length')
+    passwordSlider.addEventListener("input",()=>{passwordLength.innerText = passwordSlider.value})
+    
+    function asd(){let password = "";
+    for (let i = 0; i <passwordLength.innerText; i++) {
         let randomIndex = Math.floor(Math.random() * charters.length);
         password += charters[randomIndex];
     }
-alert(`Generated Password:  ${ password}`)
+    return password}
+    
+let passwordGenerator = document.querySelector('#password-generator')
+passwordGenerator.addEventListener('click',()=>{;document.querySelector("#password").value =asd()})
+ 
